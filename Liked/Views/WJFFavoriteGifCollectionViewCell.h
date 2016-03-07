b81283e4c894cf457878b78bd4985a71a9481342
wleofeng <wleofeng@gmail.com>
2016-03-07 18:21:30 -0500

    added working realm + collection view

---
+++ Liked/Views/WJFFavoriteGifCollectionViewCell.h
@@ -0,0 +1,13 @@
+//
+//  WJFFavoriteGifCollectionViewCell.h
+//  Liked
+//
+//  Created by Wo Jun Feng on 3/7/16.
+//  Copyright © 2016 Wo Jun Feng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WJFFavoriteGifCollectionViewCell : UICollectionViewCell
+
+@end
---
+++ Liked/Views/WJFFavoriteGifCollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  WJFFavoriteGifCollectionViewCell.m
+//  Liked
+//
+//  Created by Wo Jun Feng on 3/7/16.
+//  Copyright © 2016 Wo Jun Feng. All rights reserved.
+//
+
+#import "WJFFavoriteGifCollectionViewCell.h"
+
+@implementation WJFFavoriteGifCollectionViewCell
+
+@end
